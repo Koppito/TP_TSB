c88f004e0d0702681374ab9f5b575a2bcd0909b9
Matias <koppmatias97@gmail.com>
2017-11-10 17:33:38 -0800

    Cambiado la clase main, agregada la extension a Application

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -12,13 +12,11 @@ import javafx.stage.StageStyle;
  *
  * @author Koppito
  */
-public class Main {
+public class Main extends Application {
 
-    /*
+
     @Override
     public void start(Stage stage) throws Exception {
-        throw new Exception();
-
         Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
 
@@ -26,16 +24,10 @@ public class Main {
         
         stage.setScene(scene);
         stage.show();
-    }*/
+    }
 
     public static void main(String[] args) {
-        TSB_OAHashtable<Integer, Integer> hashTabla = new TSB_OAHashtable(25);
-        System.out.println(hashTabla.size());
-        hashTabla.put(2, 3);
-        System.out.println(hashTabla.get(2));
-        System.out.println(hashTabla.put(2, 45));
-        System.out.println(hashTabla.get(2));
-        System.out.println(hashTabla.get(3));
+        System.out.println("Error");
     }
 
 }
